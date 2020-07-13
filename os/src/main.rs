c48518d42b7ff4590c919bd1a569df785e29263b
gitstar <gy163email@163.com>
2020-07-13 22:35:22 +0800

    完成lab0的代码

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,4 +1,5 @@
 //! # 全局属性
+//!
 //! - `#![no_std]`  
 //!   禁用标准库
 #![no_std]
@@ -7,18 +8,36 @@
 //! - `#![no_main]`  
 //!   不使用 `main` 函数等全部 Rust-level 入口点来作为程序入口，不用常规的入口点
 #![no_main]
+// 汇编编写的程序入口，具体见该文件
 
-/// 覆盖 crt0 中的 _start 函数
-/// 我们暂时将它的实现为一个死循环
+//! # 一些 unstable 的功能需要在 crate 层级声明后才可以使用
+//!
+//! - `#![feature(llvm_asm)]`  
+//!   内嵌汇编
+#![feature(llvm_asm)]
+//!
+//! - `#![feature(global_asm)]`
+//!   内嵌整个汇编文件
+#![feature(global_asm)]
+//!
+//! - `#![feature(panic_info_message)]`  
+//!   panic! 时，获取其中的信息并打印
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
+
+// 汇编编写的程序入口，具体见该文件
+global_asm!(include_str!("entry.asm"));
+
+/// Rust 的入口函数
+///
+/// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle] //禁用编译期间的名称重整（Name Mangling），保证生成命为_start的函数
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
-/// 当 panic 发生时会调用该函数
-/// 我们暂时将它的实现为一个死循环
-use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+pub extern "C" fn rust_main() -> ! {
+    println!("Hello rCore-Tutorial.");
+    println!("Hello GuiYi.");
+    panic!("end of rust_main.")
 }
