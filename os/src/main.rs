f25dd6222574340a6f4b1b11d63a98d9b0f9d6f4
gystar <gy163email@163.com>
2020-07-21 19:41:29 +0800

    加入一处断言，完成lab5的代码

--- os/src/main.rs
+++ os/src/main.rs
@@ -31,6 +31,10 @@
 //! - `#![feature(slice_fill)]`
 //!   允许将 slice 填充值
 #![feature(slice_fill)]
+//! - `#![feature(naked_functions)]`
+//!   允许使用 naked 函数，即编译器不在函数前后添加出入栈操作。
+//!   这允许我们在函数中间内联汇编使用 `ret` 提前结束，而不会导致栈出现异常
+#![feature(naked_functions)]
 
 #[macro_use]
 mod console;
