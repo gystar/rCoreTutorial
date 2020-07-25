6d4193e3a928e9baabbf759812ba2178fc8877eb
gystar <gy163email@163.com>
2020-07-25 11:33:16 +0800

    更新一下两个汇编文件

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -10,7 +10,7 @@
     .globl _start
 # 目前 _start 的功能：将预留的栈空间写入 $sp，然后跳转至 rust_main
 _start:
-    # 计算 boot_page_table 的物理页号
+    # 通过线性映射关系计算 boot_page_table 的物理页号
     lui t0, %hi(boot_page_table)
     li t1, 0xffffffff00000000
     sub t0, t0, t1
@@ -22,10 +22,11 @@ _start:
     csrw satp, t0
     sfence.vma
 
-    # 加载栈地址
+    # 加载栈的虚拟地址
     lui sp, %hi(boot_stack_top)
     addi sp, sp, %lo(boot_stack_top)
     # 跳转至 rust_main
+    # 这里同时伴随 hart 和 dtb_pa 两个指针的传入（是 OpenSBI 帮我们完成的）
     lui t0, %hi(rust_main)
     addi t0, t0, %lo(rust_main)
     jr t0
